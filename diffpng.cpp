4ed71a18c8c02b2f115514060b8354b514adcc25
Don Bright <hugh.m.bright@gmail.com>
2014-06-04 22:29:56 -0500

    down to two source files

--- diffpng.cpp
+++ diffpng.cpp
@@ -23,10 +23,7 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#include "lpyramid.hpp"
-#include "rgbaimage.hpp"
-#include "compareargs.hpp"
-#include "metric.h"
+#include "metric.hpp"
 
 #include <iostream>
 #include <string>
